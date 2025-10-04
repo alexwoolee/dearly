0300ff3b2a1743f8beb96bb86188a4206a7ae1f0
Alex Lee <alexlee8937@gmail.com>
2025-10-04 15:59:29 -0700

    chore/task: add folders, home page, create files

--- dearly/src/App.jsx
+++ dearly/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
+import Home from './pages/home'
 
 function App() {
 
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+        <Home></Home>
       </div>
     </>
   )
---
+++ dearly/src/sendMail.js
@@ -0,0 +1,5 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "dearlymail2025@gmail.com",
+})
