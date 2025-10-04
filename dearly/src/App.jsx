670e7a8aae5023171304cd71dfb88eb05f262377
Alex Lee <alexlee8937@gmail.com>
2025-10-04 16:14:22 -0700

    style: temporary layout of home page

--- dearly/src/App.jsx
+++ dearly/src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import Home from './pages/home'
+import Home from './pages/Home'
 
 function App() {
-
   return (
-    <>
-      <div>
-        <Home></Home>
-      </div>
-    </>
+    <div>
+      <Home />
+    </div>
   )
 }
 
--- dearly/src/sendMail.js
+++ dearly/src/sendMail.js
@@ -1,5 +1,18 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "dearlymail2025@gmail.com",
-})
+  service: "gmail",
+  auth: {
+    user: "dearlymail2025@gmail.com",   // the Gmail you just made
+    pass: "btbu qwxu atlv xzbt", // from step 2
+  },
+});
+
+const info = await transporter.sendMail({
+  from: "dearlymail2025@gmail.com",
+  to: "diamsterac@gmail.com",        // where you want to receive
+  subject: "Dearly",
+  text: "This is a test email. Seeing this meant success",
+});
+
+console.log("Message sent:", info.messageId);
