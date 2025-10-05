29eeddcbc4e9334a502e35d90d9215edb3c894ab
Alex Lee <alexlee8937@gmail.com>
2025-10-04 22:14:41 -0700

    feat: add routing

--- dearly/src/App.jsx
+++ dearly/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import Home from './pages/Home'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home.jsx';
+import NavBar from './components/NavBar.jsx';
+import Footer from './components/Footer.jsx';
+import Step1 from './pages/StepOne.jsx';
+import Step2 from './pages/StepTwo.jsx';
+import Step3 from './pages/StepThree.jsx';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home></Home>}>
+            <Route path="/step1" element={<Step1></Step1>}></Route>
+            <Route path="/step2" element={<Step2></Step2>}></Route>
+            <Route path="/step3" element={<Step3></Step3>}></Route>
+          </Route>
+        </Routes>      
+        {/* <Footer /> */}
+      </BrowserRouter>
+    </>
   )
 }
 
